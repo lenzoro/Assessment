1da11008ee9743b4c4d552482d4e616e967b68e8
lenzoro <devoninniss148@gmail.com>
2019-03-20 16:50:40 -0300

    add button

--- client/main.js
+++ client/main.js
@@ -18,22 +18,40 @@ import '../lib/collections.js';
 // 		return userDB.find({});
 // 	}
 // });
+Template.mainBody.helpers({
+	allID(){
+		return userDB.find({})
+	}
+})
 
 Template.addbook.events({
   'click .js-save'(event, instance) {
-		var name = $('#myModal input[name="name"]').val()
-		var title = $('#myModal input[name="title"]').val()
-		var description = $('#myModal input[name="description"]').val()
-		var img = $('#myModal input[name="image"]').val()
-		if(img ==""){
-			img="https://cdn2.vectorstock.com/i/1000x1000/57/36/book-icon-isolated-on-round-background-vector-15985736.jpg"
+		var name = $('#myModal input[name="name"]').val();
+		var title = $('#myModal input[name="title"]').val();
+		var description = $('#myModal input[name="Description"]').val();
+		var img = $('#myModal input[name="image"]').val();
+		if(img ==undefined || img == ""){
+			img="book.png";
 		}
 		console.log("The name is",name,title);
-		$('#myModal input[name="name"]').val('')
-		$('#myModal input[name="title"]').val('')
-		$('#myModal input[name="image"]').val('')
+		$('#myModal input[name="name"]').val('');
+		$('#myModal input[name="title"]').val('');
+		$('#myModal input[name="image"]').val('');
+		$('#myModal input[name="Description"]').val('');
 		$('#myModal').modal('hide');
-		userDB.insert({'name':name,'title':title, 'image':img});
+		userDB.insert({'name':name,'title':title, 'image':img,'des':description});
+
   		
   },
+  
+});
+Template.mainBody.events({
+  'click .edit-js'(event, instance){
+  	console.log("edit clicked");
+  	$('#modaledit').modal('show');
+
+  },
+  'click .js-tdown'(event, instance){
+  	var likes = userDB.findone({})
+  }
 });
